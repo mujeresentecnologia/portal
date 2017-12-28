402e4813df0061ea6220676091053a4f10f43ef2
Aninha Lacerda <alacerda@thoughtworks.com>
2017-12-28 15:59:49 -0300

    Create unit test for description

--- _spec/seo_spec.rb
+++ _spec/seo_spec.rb
@@ -3,13 +3,20 @@ require 'rspec'
 describe 'SEO Metadatos' do
   context 'when title and description are defined on _config' do
     let(:doc) { load_home }
-    let(:title_content) { load_main_config().fetch('title') }
+    let(:config) { load_main_config }
 
     it 'define a tag <title> inside <head>' do
-      # byebug
+      title_content = config.fetch('title')
       title_tag = doc.xpath('//title')
 
       expect(title_tag.text).to eql(title_content)
+    end
+
+    it 'define a tag <meta description> inside <head>' do
+      desc_content = config.fetch('description')
+      desc_tag = doc.xpath('//html/head/meta[@name="description"]')
+
+      expect(desc_tag[0]['content']).to eql(desc_content)
     end
   end
 end
