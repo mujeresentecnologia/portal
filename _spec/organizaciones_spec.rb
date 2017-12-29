fbd9fe0125841b7029a6676dd99d099083127c1f
Aninha Lacerda <alacerda@thoughtworks.com>
2017-12-29 09:55:48 -0300

    Add unit tests to check logos on the home page

--- _spec/organizaciones_spec.rb
+++ _spec/organizaciones_spec.rb
@@ -2,7 +2,6 @@ require 'rspec'
 
 describe 'Organizaciones' do
   context 'cuando organizaciones estan definidas en el archivo _data/organizaciones.yml' do
-
     let(:organizaciones) { load_data('organizaciones') }
     let(:org_logos) { organizaciones.map {|org| org['logo']} }
 
@@ -13,6 +12,21 @@ describe 'Organizaciones' do
             path = File.join('./assets/images/', logo)
             File.file?(path)
           end)
+      end
+    end
+
+    context 'cuando mirando la pagina inicial' do
+      let(:home) { load_home }
+      let(:organizaciones) { load_data('organizaciones') }
+
+      it "los logos de todas las organizaciones deben estar en la pagina inicial" do
+        organizaciones.each do |org|
+          element_src = home.css('.met_logos').xpath(%Q{//img[@src="assets/images/#{org['logo']}"]})
+          element_alt = home.css('.met_logos').xpath(%Q{//img[@alt="#{org['nombre']}"]})
+
+          expect(element_src.size).to eq(1)
+          expect(element_alt.size).to eq(1)
+        end
       end
     end
   end
