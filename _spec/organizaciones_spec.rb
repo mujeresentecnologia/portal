267d5a0473e310e09c1690c7290f50ded4668c5f
Aninha Lacerda <alacerda@thoughtworks.com>
2018-01-04 13:28:35 -0300

    [#41] Fill organizacions logos dinamically, reading from data file once again

--- _spec/organizaciones_spec.rb
+++ _spec/organizaciones_spec.rb
@@ -18,6 +18,7 @@ describe 'Organizaciones' do
     context 'cuando mirando la pagina inicial' do
       let(:home) { load_home }
       let(:organizaciones) { load_data('organizaciones') }
+      let(:grid_styles) {load_data('structure/grid_styles')}
 
       it "los logos de todas las organizaciones deben estar en la pagina inicial" do
         organizaciones.each do |org|
@@ -26,6 +27,15 @@ describe 'Organizaciones' do
 
           expect(element_src.size).to eq(1)
           expect(element_alt.size).to eq(1)
+        end
+      end
+
+      it "los logos deben estar ordenados de acuerdo con la posicion determinada en el archivo" do
+        organizaciones.each do |org|
+          element_src = home.css('.met_logos').xpath(%Q{//img[@src="assets/images/#{org['logo']}"]})
+          element_class = element_src[0].parent.attr('class')
+
+          expect(element_class).to eq(grid_styles["#{org['position']}"][0]['style'])
         end
       end
     end
