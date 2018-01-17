e2bf68db15208277e21b9c164e0b11c23230e230
vickyFR11 <vfuenmay@thoughtworks.com>
2018-01-17 17:30:40 -0300

    [Tech-Task] Fixing tests for about page

--- _spec/about_spec.rb
+++ _spec/about_spec.rb
@@ -5,12 +5,30 @@ describe 'Content in About Page' do
     let(:about) { load_page("about") }
     let(:markdown) { load_markdown("about") }
 
+    it "I can read the title of MET" do
+      content = about.css('.met_dark').xpath(%Q{//h1})
+
+      title = content[0]
+
+      expect(title.text).to match(markdown["title"])
+
+    end
+
     it "I can read the first paragraph of MET's description" do
       paragraphs = about.css('.met_dark').xpath(%Q{//p})
 
       first_paragraph = paragraphs[0]
-      expect(markdown["first_paragraph"]).to match(first_paragraph)
+      expect(first_paragraph.text).to match(markdown["first_paragraph"])
 
     end
+
+    it "I can read the second paragraph of MET's description" do
+      paragraphs = about.css('.met_dark').xpath(%Q{//p})
+
+      second_paragraph = paragraphs[1]
+      expect(second_paragraph.text).to match(markdown["second_paragraph"])
+    end
+
+
   end
 end
