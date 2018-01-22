dfcc6866440707238a829637faa8291345eb99de
vickyFR11 <vfuenmay@thoughtworks.com>
2018-01-22 10:32:58 -0300

    [#24] Adding type feature to capybara helper

--- _spec/post_spec.rb
+++ _spec/post_spec.rb
@@ -1,29 +1,43 @@
 require 'rspec'
+require 'capybara_helper'
+require 'yaml'
 
-describe "Drafts" do
+describe "Drafts", :type => :feature do
   let(:drafts_files) { load_drafts }
   it "drafts should only be generated for staging environment" do
     if ENV['ENTORNO'] == "staging-env"; then
-		expect(drafts_files).to all(
+		  expect(drafts_files).to all(
         satisfy do |file|
 
-	      post_title = File.basename(file, ".md")
-	      post_data = load_markdown("_drafts/#{post_title}")
-	      post_page = load_page(post_title)
+          post_title = File.basename(file, ".md")
+          post_data = load_markdown("_drafts/#{post_title}")
+          post_page = load_page(post_title)
 
-	      expect(post_page.xpath('//h1').text).to eq(post_data["title"])
-	    end)
+          expect(post_page.xpath('//h1').text).to eq(post_data["title"])
+        end)
 	  end
   end
 
   it "drafts should not be generated for production environment" do
     if ENV['ENTORNO'] != "staging-env"; then
-		expect(drafts_files).to all(
-			satisfy do |file|
+		  expect(drafts_files).to all(
+			  satisfy do |file|
 
-		  post_title = File.basename(file, ".md")
-	    expect(File).not_to exist("_site/#{post_title}.html")
-	  end)
+		    post_title = File.basename(file, ".md")
+	      expect(File).not_to exist("_site/#{post_title}.html")
+	    end)
+    end
+  end
+
+  it "url of drafts should be like file name" do
+    if ENV['ENTORNO'] != "staging-env"; then
+		  expect(drafts_files).to all(
+			  satisfy do |file|
+        
+        file_name = File.basename(file, ".md")
+        visit "/" + file_name + ".html"
+        expect(page.status_code).to eq(200)
+      end)
     end
   end
 end
