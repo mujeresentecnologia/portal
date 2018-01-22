83135cbde33b9452b70c48624f7308c4d7942dfc
vickyFR11 <vfuenmay@thoughtworks.com>
2018-01-22 12:23:07 -0300

    [#24] Adding logo from organization to post

--- _spec/post_spec.rb
+++ _spec/post_spec.rb
@@ -3,7 +3,8 @@ require 'capybara_helper'
 require 'yaml'
 
 describe "Drafts", :type => :feature do
-  let(:drafts_files) { load_drafts }
+  let(:drafts_files) { load_all_drafts }
+  let(:organizations) { load_data("organizaciones") }
   it "drafts should only be generated for staging environment" do
     if ENV['ENTORNO'] == "staging-env"; then
 		  expect(drafts_files).to all(
@@ -40,11 +41,28 @@ describe "Drafts", :type => :feature do
       end)
     end
   end
+
+  it "all posts are generated with the correct logo from the organization" do
+    expect(drafts_files).to all(
+		  satisfy do |file|
+
+      post_title = File.basename(file, ".md")
+      post_data = load_markdown("_drafts/#{post_title}")
+      post_page = load_page(post_title)
+      id = post_data["organization_id"]
+      expected_img_alt = organizations[id]['name']
+
+      image_tag = post_page.css("img[alt='#{organizations[id]['name']}']")
+      img_alt_from_post = image_tag[0].attributes["alt"].text
+
+      expect(img_alt_from_post).to match(expected_img_alt)
+    end)
+  end
 end
 
 describe "Posts" do
   it "all posts are generated with correct title" do
-    posts_files = load_posts
+    posts_files = load_all_posts
 
 	  expect(posts_files).to all(
 		  satisfy do |file|
