f7fd6de3183561bdcecd290dd4c43a4ab2d52fb0
Aninha Lacerda <alacerda@thoughtworks.com>
2018-01-22 14:19:53 -0300

    [#25] use organization_id on posts instead of published_by

--- _spec/post_spec.rb
+++ _spec/post_spec.rb
@@ -43,35 +43,39 @@ describe "Drafts", :type => :feature do
   end
 
   it "all posts are generated with the correct logo from the organization" do
-    expect(drafts_files).to all(
+    if ENV['ENTORNO'] == "staging-env"; then
+      expect(drafts_files).to all(
 		  satisfy do |file|
 
-      post_title = File.basename(file, ".md")
-      post_data = load_markdown("_drafts/#{post_title}")
-      post_page = load_page(post_title)
-      id = post_data["organization_id"]
-      expected_img_alt = organizations[id]['name']
+        post_title = File.basename(file, ".md")
+        post_data = load_markdown("_drafts/#{post_title}")
+        post_page = load_page(post_title)
+        id = post_data["organization_id"]
+        expected_img_alt = organizations[id]['name']
 
-      image_tag = post_page.css("img[alt='#{organizations[id]['name']}']")
-      img_alt_from_post = image_tag[0].attributes["alt"].text
+        image_tag = post_page.css("img[alt='#{organizations[id]['name']}']")
+        img_alt_from_post = image_tag[0].attributes["alt"].text
 
-      expect(img_alt_from_post).to match(expected_img_alt)
-    end)
+        expect(img_alt_from_post).to match(expected_img_alt)
+      end)
+    end
   end
 end
 
 describe "Posts" do
   it "all posts are generated with correct title" do
-    posts_files = load_all_posts
+    if ENV['ENTORNO'] != "staging-env"; then
+      posts_files = load_all_posts
 
 	  expect(posts_files).to all(
 		  satisfy do |file|
 
-      post_title = File.basename(file, ".md")
-      post_data = load_markdown("_posts/#{post_title}")
-      post_page = load_page(post_title)
+        post_title = File.basename(file, ".md")
+        post_data = load_markdown("_posts/#{post_title}")
+        post_page = load_page(post_title)
 
-      expect(post_page.xpath('//h1').text).to eq(post_data["title"])
-    end)
+        expect(post_page.xpath('//h1').text).to eq(post_data["title"])
+      end)
+    end
   end
 end
