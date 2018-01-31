6c145e7abeacab8a6c9202b4ba27a9fb3038f307
Javier Villalobos A <jvillalo@thoughtworks.com>
2018-01-31 15:26:30 -0300

    [#26] Adding toggle feature for publications in home

--- _spec/last_three_posts_spec.rb
+++ _spec/last_three_posts_spec.rb
@@ -1,8 +1,9 @@
 describe 'All posts' do
 	context 'I am in Home Page' do
 		let(:posts_files) { load_all_posts }
+		let(:toggles) { load_data("toggles") }
 		it 'and I can see the last three posts' do
-			if ENV['ENTORNO'] != "staging-env"; then
+			if ENV['ENTORNO'] != "staging-env" && toggles["home"]["last_three_posts"] ; then
 				last_three_posts = posts_files.sort.reverse.first(3)
 				home_page = load_home
 				expected_posts_titles = home_page.css('.met_light h1')
